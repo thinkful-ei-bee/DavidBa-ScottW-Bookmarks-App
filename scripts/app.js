96968b1ac6c0b203919eb8d7c7621958f48768ba
Scott Williams <scottcolemanwilliams@gmail.com>
2019-03-05 16:25:24 -0600

    prepped app.js for event listeners and render function

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+// eslint-disable-next-line no-unused-vars
+const app = (function() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function bindEventListeners() {
+
+  }
+  return {
+    render,
+    bindEventListeners
+  };
+
+})();
--- scripts/store.js
+++ scripts/store.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// eslint-disable-next-line no-unused-vars
 const store = (function(){
 
   const addItem = function(item){
