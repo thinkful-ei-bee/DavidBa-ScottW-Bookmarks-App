361ce24ac29563f9f5c7048cc8c0ed2c2edc6a29
Scott Williams <scottcolemanwilliams@gmail.com>
2019-03-06 11:35:41 -0600

    added handleAddBookmark js handleSubmitNewBookmark js moved newBookmark html to main

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,42 @@
 'use strict';
+/* global store, item */
 
 // eslint-disable-next-line no-unused-vars
 const app = (function() {
 
 
+
+  function render() {
+
+    if (store.isAdding) {
+      $('#js-main-buttons').addClass('hidden');
+      $('.js-adding-item-container').removeClass('hidden');
+      //insert add bookmark html
+    }
+  }
+
+  function handleAddBookmark() {
+    $('.js-add-bookmark').on('click', () => {
+      store.toggleIsAdding();
+      render();
+    });
+  }
+
+  function handleSubmitNewBookmark() {
+    $('.js-adding-item-container').on('click', '#js-submit-bookmark', (event) => {
+      event.preventDefault();
+
+      const title = $('#js-set-title').val();
+      const url = $('#js-set-url').val();
+      const desc = $('#js-set-desc').val();
+      const rating = $('input[name=js-set-rating]:checked', '.set-rating').val();
+
+      const newItem = item.create(title, url, desc, rating);
+      store.addItem(newItem);
+      store.toggleIsAdding();
+      render();
+    });
+  }
 
 
 
@@ -16,13 +49,14 @@ const app = (function() {
 
 
   function bindEventListeners() {
-
+    handleAddBookmark();
+    handleSubmitNewBookmark();
   }
   return {
     render,
     bindEventListeners
   };
-
+ 
 })();
 
 
